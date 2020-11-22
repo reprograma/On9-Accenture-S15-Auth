02d9244094153302490d16db477b866d9d68e10a
gloriateodoro <gloriateodoro1@gmail.com>
2020-11-21 23:39:31 -0300

    Pastas e arquivos que serão commitados separadamente posteriormente

---
+++ exercicio/src/config/auth.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secret: "6d440767a47d2128e8e88c4f168f00c8",
+    expiresIn: '5d'
+};
---
+++ exercicio/src/routes/sessionRoute.js
@@ -0,0 +1,8 @@
+/* Verifica e chama o accessToken*/
+const express = require("express")
+const router = express.Router()
+const controller = require("../controllers/sessionController")
+
+router.post("/", controller.accessToken)
+
+module.exports = router
